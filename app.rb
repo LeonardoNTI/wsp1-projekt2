96dba8b62a3745bb47945f951f3fe6a414740f97
LeonardoNTI <leonardo.giannini@elev.ga.ntig.se>
2024-11-11 14:52:29 +0100

    FUNGERANDE
    LESGOO DE FUNKAR! Jobba med SQL nästa gång!

--- app.rb
+++ app.rb
@@ -1,32 +1,16 @@
-# app.rb
-require 'sinatra'
-require 'sqlite3'
-
-# Skapa/anslut till en SQLite-databas
-DB = SQLite3::Database.new "db/todos.db"
-
-# Skapa tabellen om den inte redan finns
-DB.execute <<-SQL
-  CREATE TABLE IF NOT EXISTS todos (
-    id INTEGER PRIMARY KEY,
-    title TEXT,
-    description TEXT,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-  );
-SQL
-
 class App < Sinatra::Base
-  get '/' do
-    # Hämta alla ToDos från databasen
-    @todos = DB.execute("SELECT * FROM todos")
-    erb :index
+  
+  def db
+    return @db if @db
+
+    @db = SQLite3::Database.new("db/todos.sqlite")
+    @db.results_as_hash = true
+
+    return @db
   end
 
-  post '/todos' do
-    # Lägg till en ny ToDo i databasen
-    DB.execute("INSERT INTO todos (title, description) VALUES (?, ?)", [params[:title], params[:description]])
-    redirect '/'
+  get '/' do 
+    @todos = db.execute('SELECT * FROM todos')
+    erb(:"index")
   end
-
-  run! if app_file == $0
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,12 +1,36 @@
-# db/seeder.rb
 require 'sqlite3'
 
-DB = SQLite3::Database.new "db/todos.db"
+class Seeder
+  
+  def self.seed!
+    drop_tables
+    create_tables
+    populate_tables
+  end
 
-# Exempel på seed-data
-DB.execute("INSERT INTO todos (title, description) VALUES (?, ?)", ["Lära mig Ruby", "Kolla igenom alla viktiga delar av Ruby"])
-DB.execute("INSERT INTO todos (title, description) VALUES (?, ?)", ["Bygga en webbapp", "Använda Sinatra och SQLite för att bygga en enkel webbapp"])
-DB.execute("INSERT INTO todos (title, description) VALUES (?, ?)", ["Implementera CSS", "Gör designen snygg med CSS"])
-DB.execute("INSERT INTO todos (title, description) VALUES (?, ?)", ["Lägga till interaktivitet", "Bygga dynamisk funktionalitet med JavaScript eller formulär i HTML"])
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS todos')
+  end
 
-puts "Databasen har fyllts med seed-data!"
+  def self.create_tables
+    db.execute('CREATE TABLE todos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                description TEXT)')
+  end
+
+  def self.populate_tables
+   db.execute('INSERT INTO todos (name, description) VALUES ("Lära dig stava", "Gå till skolan dumfan")')
+  end
+
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/todos.sqlite')
+    @db.results_as_hash = true
+    @db
+  end
+end
+
+
+Seeder.seed!
